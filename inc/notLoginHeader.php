fa9e6564644235f24530d07e65996b1465c27319
赤堀翼 <akahoritsubasa@Mac-A.local>
2024-09-24 21:47:12 +0900

    パスの変更

--- inc/notLoginHeader.php
+++ inc/notLoginHeader.php
@@ -3,10 +3,10 @@
     <label for="menu-btn-check" class="menu-btn"><span></span></label>
     <div class="menu-content">
         <ul class="ddmenu">
-            <li><a href="../index.php"><img src="../library/shopping.png" alt="買い物">商品の購入</a></li>
-            <li><a href="../login.php"><img src="../library/login.png" alt="ログイン">ログイン</a></li>
+            <li><a href="../index.php"><img src="/rice-project/library/shopping.png" alt="買い物">商品の購入</a></li>
+            <li><a href="../login.php"><img src="./../library/login.png" alt="ログイン">ログイン</a></li>
             <li><a href="../company.php"><img src="../library/company.png" alt="会社">会社情報</a></li>
-            <li><a href="../inquiry.php"><img src="../library/phone.png" alt="お問い合わせ">お問い合わせ</a></li>
+            <li><a href="../inquiry.php"><img src="../phone.png" alt="お問い合わせ">お問い合わせ</a></li>
         </ul>
     </div>
 </div>
