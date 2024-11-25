a6524ef4a28dc49812e9e17f12e08923d0cb67ef
赤堀翼 <akahoritsubasa@Mac-A.local>
2024-11-25 19:53:10 +0900

    110行目if文にelseを追加

--- public/mypage.php
+++ public/mypage.php
@@ -111,13 +111,15 @@ $oneWeekLaterNowTimeStamp = $howToLogin->getOneWeekLaterDateTimeStamp();
             <div class="admin-page">
                 <a href="admin/index.php">商品の追加・削除 、 又は休日の追加・削除はこちら</a>
             </div>
-        <?php endif ?>
+        <?php else: ?>
         <div class="notes">
             <ul>
                 <li>※支払い方法、現金のみ</li>
                 <li>※購入キャンセル、お届け日の前日まで</li>
                 <li>※当日でのキャンセルは、<a href="inquiry.php">ご連絡ください</a></li>
             </ul>
+        </div>
+        <?php endif ?>
         <div class="purchase-history">
             <p style="color: red;"><?= isset($_SESSION[ConstApp::SIGNUP_MESSAGE][ConstApp::MYPAGE]) && !empty($_SESSION[ConstApp::SIGNUP_MESSAGE][ConstApp::MYPAGE]) ? $_SESSION[ConstApp::SIGNUP_MESSAGE][ConstApp::MYPAGE] : '' ?></p>
             <tables>
