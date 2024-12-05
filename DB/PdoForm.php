5740cf3fc3a7901dff6d8a7a7c09755d77752263
赤堀翼 <akahoritsubasa@Mac-A.local>
2024-12-05 11:36:55 +0900

    データベース接続部分のポート番号追記・password追記

--- DB/PdoForm.php
+++ DB/PdoForm.php
@@ -12,17 +12,16 @@ trait PdoForm
     // サーバー用
     public function connect()
     {
-        // try {
-            // $pdo = new PDO('mysql:host=us-cluster-east-01.k8s.cleardb.net;dbname=heroku_65df047e8aa26c8;charset=utf8mb4', 'bd0defb290d916', '5a2e514f');
-            $pdo = new PDO('pgsql:host=c9mq4861d16jlm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com;dbname=d3g2ci5qnuh4l2', 'ulappdaufofjf', 'your_password_here');
+        try {
+            $pdo = new PDO('pgsql:host=c9mq4861d16jlm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com;port=5432;dbname=d3g2ci5qnuh4l2', 'ulappdaufofjf', 'your_password_here');
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $pdo->setAttribute(PDO::MYSQL_ATTR_MULTI_STATEMENTS, false);
-        // } catch(PDOException $e){
-        //     file_put_contents(__DIR__. '/../errorLog/DBError.php', $this->objDateTime->format('Y-m-d H:i:s'). ', 異常名 '. $e->getLine(). $e->getMessage(). "\n", FILE_APPEND | LOCK_EX);
-        //     header('Location: error.php');
-        //     exit();
-        // }
+        } catch(PDOException $e){
+            file_put_contents(__DIR__. '/../errorLog/DBError.php', $this->objDateTime->format('Y-m-d H:i:s'). ', 異常名 '. $e->getLine(). $e->getMessage(). "\n", FILE_APPEND | LOCK_EX);
+            header('Location: error.php');
+            exit();
+        }
         return $pdo;
     }
 
@@ -42,4 +41,9 @@ trait PdoForm
     //     }
     //     return $pdo;
     // }
+
+
+    // ClearDB(サーバー用)
+    // $pdo = new PDO('mysql:host=us-cluster-east-01.k8s.cleardb.net;dbname=heroku_65df047e8aa26c8;charset=utf8mb4', 'bd0defb290d916', '5a2e514f');
+
 }
